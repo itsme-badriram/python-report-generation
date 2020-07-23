1905d4b44e6ccaa4aff3f0b99cb5670cf47a2568
V. Ramanujan <42889791+itsme-badriram@users.noreply.github.com>
2020-07-23 19:43:20 +0530

    Update generate_report.py

--- generate_report.py
+++ generate_report.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
+#Read the cs file
 df = pd.read_csv('report.csv')
 import jinja2
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
+#HTML template I use is 'pdf.html'
 TEMPLATE_FILE = "pdf.html"
 template = templateEnv.get_template(TEMPLATE_FILE)
 outputText = template.render(df=df)
 html_file = open('result' + '.html', 'w')
 html_file.write(outputText)
 html_file.close()
-
+#Convert the HTML file(result.html) generated to PDF.
 import pdfkit
+#This configuration is not required if the required modules are added to the path as environment variables.
 config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
 pdfkit.from_file('result.html', 'result.pdf', configuration=config)
